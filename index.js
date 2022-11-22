a693ea73283180935d27130ba28c694a8ea3f7b1
reaganongaya <reagan.ongaya@student.moringaschool.com>
2022-11-22 23:03:03 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+function beverages() {
+    return 'coffee';
+}
+console.log(beverages());
+
+
+const softdrinks = function() {
+    return 'SodaFanta';
+}
+console.log(softdrinks());
+
+const difference = 2 - 1 ;
+console.log(difference);
+
+const sum = (parameter1, parameter2) => {
+    console.log('Adding ${parameter1}');
+    console.log('Adding ${parameter2}');
+    return parameter1 + parameter2;
+}
+sum('4,5')
+
+
